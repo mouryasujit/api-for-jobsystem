1a762253e2d0493da7c378693a9d3cdeffd9ae86
mouryasujit <mouryasujit04@gmail.com>
2023-04-30 08:50:19 +0530

    Adding review file

--- server.js
+++ server.js
@@ -25,6 +25,7 @@ const connect = async () => {
 };
 
 app.use(cors({ origin: "https://jobathon.onrender.com", credentials: true }));
+// app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 
